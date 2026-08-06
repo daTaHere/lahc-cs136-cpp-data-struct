3eb741f2f89270edcf7995a9e91221e6a3b38360
daTahere <adamhuy123@yahoo.com>
2026-08-05 18:42:05 -0700

    fixes respones output menu 3 and 4

--- cs136-data-structure/Cs136Code/21.2videoLinkedListTypeImp.cpp
+++ cs136-data-structure/Cs136Code/21.2videoLinkedListTypeImp.cpp
@@ -1,6 +1,133 @@
-#include <iostream>
+#include <iostream> 
 #include <fstream>
+#include <list>
+#include <algorithm>
+#include <iterator>
+
 #include "21.2videoLinkedListType.h"
+
+using namespace std;
+
+void videoListType::searchVideoList(string title, bool& found,
+    list<videoType>::iterator& location)
+{
+    found = false;   //set found to false
+
+    location = find(videoList.begin(), videoList.end(), title);
+
+    if (location != videoList.end())
+        found = true;
+
+}//end searchVideoList
+
+
+bool videoListType::isVideoAvailable(string title)
+{
+    bool found;
+    list<videoType>::iterator location;
+
+    searchVideoList(title, found, location);
+
+    if (found)
+        found = ((*location).getNoOfCopiesInStock() > 0);
+    else
+        found = false;
+
+    return found;
+}
+
+void videoListType::videoCheckIn(string title)
+{
+    bool found = false;
+    list<videoType>::iterator location;;
+
+    searchVideoList(title, found, location);  //search the list
+
+    if (found)
+    {
+        (*location).checkIn();
+        cout << "Thanks for returing: " << title << endl;
+    }
+    else
+        cout << "The movie selected is invalid!" << endl;
+}
+
+void videoListType::videoCheckOut(string title)
+{
+    bool found = false;
+    list<videoType>::iterator location;
+
+    searchVideoList(title, found, location);  //search the list
+
+    if (found) {
+        (*location).checkOut();
+        cout << "Enjoy your movie: " << title << endl;
+    }        
+    else
+        cout << "Video not in stock " << endl;
+}
+
+bool videoListType::videoCheckTitle(string title)
+{
+    bool found = false;
+
+    list<videoType>::iterator location;
+
+    searchVideoList(title, found, location); //search the list
+
+    return found;
+}
+
+void videoListType::videoUpdateInStock(string title, int num)
+{
+    bool found = false;
+    list<videoType>::iterator location;
+
+    searchVideoList(title, found, location); //search the list
+
+    if (found)
+        (*location).updateInStock(num);
+    else
+        cout << "Video not in stock " << endl;
+}
+
+void videoListType::videoSetCopiesInStock(string title, int num)
+{
+    bool found = false;
+    list<videoType>::iterator location;
+
+    searchVideoList(title, found, location);
+
+    if (found)
+        (*location).setCopiesInStock(num);
+    else
+        cout << "Video not in stock " << endl;
+}
+
+bool videoListType::videoSearch(string title)
+{
+    bool found = false;
+    list<videoType>::iterator location;
+
+    location = find(videoList.begin(), videoList.end(), title);
+
+    if (location != videoList.end())
+        found = true;
+
+    return found;
+}
+
+void videoListType::videoPrintTitle()
+{
+    list<videoType>::iterator location;
+
+    location = videoList.begin();
+    while (location != videoList.end())
+    {
+        (*location).printTitle();
+        location++;
+    }
+}
 
 void videoListType::insert(const videoType& item) {
     videoList.push_back(item);
@@ -12,9 +139,17 @@ void videoListType::print() const {
     }
 }
 
+string inputHelper() {
+    string input;
+    cout << "Enter Title: ";
+    getline(cin >> ws, input);
+    return input;
+}
+
 void menuHelper(videoListType& videos) {
 
     int choice = 0;
+    string title;
 
     while (choice != 9)
     {
@@ -29,22 +164,39 @@ void menuHelper(videoListType& videos) {
         cout << "\nEnter choice: ";
 
         cin >> choice;
-
         switch (choice)
         {
         case 1:
+            title = inputHelper();
+            if (videos.videoSearch(title))
+                cout << title << " was found in store.\n";
+            else
+                cout << title << " was not found in store.";
+            
             break;
 
         case 2:
+            title = inputHelper();
+            videos.videoCheckOut(title);
             break;
 
         case 3:
+            title = inputHelper();
+            videos.videoCheckIn(title);
             break;
 
         case 4:
+            title = inputHelper();
+
+            if (videos.isVideoAvailable(title))
+                cout << "Currently in stock." << endl;
+            else
+                cout << "Video not in store" << endl;
+
             break;
 
         case 5:
+            videos.videoPrintTitle();
             break;
 
         case 6:
@@ -59,10 +211,10 @@ void menuHelper(videoListType& videos) {
             cout << "Invalid choice. Try again." << endl;
         }
     }
-}
+};
 
 void Test21_2()
-{
+{    
     string title;
     string star1;
     string star2;
